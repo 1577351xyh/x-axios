5c502e404339b8670cc797bd52aab9cabcc10dd7
1577351xyh <564146958@qq.com>
2020-03-24 22:45:31 +0800

    响应头处理

--- src/error.js
+++ src/error.js
@@ -1,8 +1,19 @@
 export default function (xhr) {
+  //  响应头处理
+  let arr = xhr.getAllResponseHeaders().split('\r\n')
+  let headers = {};
+  arr.forEach(element => {
+    if (!element) return;
+    let [name, val] = element.split(': ');
+    headers[name] = val
+  });
+
   return {
     ok: false,
     data: xhr.response,
     status: xhr.status,
-    statusText: xhr.statusText
+    statusText: xhr.statusText,
+    headers: xhr.getAllResponseHeaders(),
+    xhr,
   }
 }
